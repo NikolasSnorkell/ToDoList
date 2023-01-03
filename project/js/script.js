b6919e22d00446174c4a1c3eedcf5541d46d4676
NikolasSnorkell <pushok_nik@mail.ru>
2023-01-03 15:48:51 +0300

    Added mark done func

--- project/js/script.js
+++ project/js/script.js
@@ -3,13 +3,26 @@
 
 
 new Sortable(document.querySelector('#main__todo_block'), {
-    delay: 2,
-    dropBubble: true,
-   
-  
-    animation: 0,
-    easing: "cubic-bezier(1, 0, 0, 1)",
     
+    sortable: true,
+    dropBubble: true,
+    direction: 'vertical',
+    filter: ".sortable__block .done",
+    dragClass: "sortable__block",
+    animation: 300,
+    easing: "cubic-bezier(1, 0, 0, 1)",
+    onSort: function (/**Event*/evt) {
+        var itemEl = evt.item;  // dragged HTMLElement
+        evt.to;    // target list
+        evt.from;  // previous list
+        evt.oldIndex;  // element's old index within old parent
+        evt.newIndex=2;  // element's new index within new parent
+        evt.clone // the clone element
+        evt.pullMode;  // when item is in another sortable: `"clone"` if cloning, `true` if moving
+      
+        console.log(evt.newIndex);
+    
+    },
 });
 
 
@@ -19,10 +32,13 @@ function showTodos(arr){
     $('#main__todo_block').html('');
 
     for([index,todo] of arr.entries()){
-        $('#main__todo_block').html($('#main__todo_block').html()+`<div class="todoitem" name="${index}">
-        ${todo}
+        $('#main__todo_block').html($('#main__todo_block').html()+`<div class="sortable__block"><div class="todoitem" name="${index}">
+        <div class="todoitem__check_block">
+        <input type="checkbox" class="todoitem__check" id="todo${index}">
+        <label for="todo${index}" class="todoitem__label">${todo}</label>
+        </div>
         <img src="img/trash.png" class="todoitem__trash" onclick="delItem(this)" alt="trash">
-    </div>`);
+    </div></div>`);
     }
 }
 
@@ -43,4 +59,12 @@ function delItem(elem){
     let par_id = $(elem).parent("div").attr("name");
    todosCurrentObj.splice(par_id,1);
     showTodos(todosCurrentObj);
+    console.log(todosCurrentObj);
 }
+
+$('.todoitem__label').click(function(){
+    $(this).toggleClass("checked");
+    $(this).parent('div').parent(".todoitem").toggleClass("done");
+})
+
+
