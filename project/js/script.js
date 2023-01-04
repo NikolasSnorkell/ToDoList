ae893979566e413e8c78568762a0b5593126d2da
NikolasSnorkell <pushok_nik@mail.ru>
2023-01-04 14:04:33 +0300

    Mark items functionality added

--- project/js/script.js
+++ project/js/script.js
@@ -8,6 +8,8 @@ new Sortable(document.querySelector('#main__todo_block'), {
     dropBubble: true,
     direction: 'vertical',
     filter: ".sortable__block .done",
+    forceFallback: true,
+    fallbackClass: "sortable__block",
     dragClass: "sortable__block",
     animation: 300,
     easing: "cubic-bezier(1, 0, 0, 1)",
@@ -25,26 +27,56 @@ new Sortable(document.querySelector('#main__todo_block'), {
     },
 });
 
+let todosActive = [], todosDone = [];
+let todosCurrentObj = [];
 
-let todosCurrentObj = ["Draw the vehicle","Wash the dishes"];
+// функция показа всех имеющихся дел, как сделанных так и активных
 
-function showTodos(arr){
+var showTodos = (arr) =>{
+
+
     $('#main__todo_block').html('');
 
     for([index,todo] of arr.entries()){
-        $('#main__todo_block').html($('#main__todo_block').html()+`<div class="sortable__block"><div class="todoitem" name="${index}">
+        let class__check,class__item,shift = 0; // 1,2 классы которые добавляются если дело сделано; 3 это сдвиг для переборки чтобы сделанные дела начинались с 0 для совпадения с элементами массива
+
+// проверка статуса дела\
+        if(todo.status=="done"){
+            class__check="checked";
+            class__item="done";
+            shift = todosActive.length; // сдвиг на длину массива активных дел
+        } else {
+            class__check="";
+            class__item="";
+            shift = 0;
+        }
+
+        $('#main__todo_block').html($('#main__todo_block').html()+`<div class="sortable__block"><div class="todoitem ${class__item}" name="${index-shift}">
         <div class="todoitem__check_block">
         <input type="checkbox" class="todoitem__check" id="todo${index}">
-        <label for="todo${index}" class="todoitem__label">${todo}</label>
+        <label for="todo${index}" class="todoitem__label ${class__check}" onclick="markItem(this)">${todo.body}</label>
         </div>
         <img src="img/trash.png" class="todoitem__trash" onclick="delItem(this)" alt="trash">
     </div></div>`);
+
+
     }
 }
 
-showTodos(todosCurrentObj)
+todosActive = [  {body:"Draw the vehicle", status:"active"},
+{body:"Wash the dishes", status:"active"}]
+todosDone = [{
+    body:"Create project",status:"done"
+},];
 
 
+
+showTodos([...todosActive,...todosDone])
+
+
+
+
+// слушатель для добавления нового дела в список
 $('#addToDo').click(function(){
     let toDoText = $('#typeToDo').val();
     $('#typeToDo').val("");
@@ -53,18 +85,34 @@ $('#addToDo').click(function(){
     showTodos(todosCurrentObj);
 })
 
-
+// функция удаления дела
 function delItem(elem){
        
     let par_id = $(elem).parent("div").attr("name");
    todosCurrentObj.splice(par_id,1);
     showTodos(todosCurrentObj);
-    console.log(todosCurrentObj);
+    console.log(todosCurrentObj); 
 }
 
-$('.todoitem__label').click(function(){
-    $(this).toggleClass("checked");
-    $(this).parent('div').parent(".todoitem").toggleClass("done");
-})
 
+// функция отметки сделанного дела
+function markItem(elem){
+    let todoIndex = $(elem).parent('div').parent(".todoitem").attr("name");
+    if($(elem).hasClass('checked')) transferItem(todosDone,todoIndex,todosActive,"active");
+    else  transferItem(todosActive,todoIndex,todosDone,"done");
+
+    $(elem).toggleClass("checked");
+    $(elem).parent('div').parent(".todoitem").toggleClass("done");
+}
+
+
+// функция для 
+function transferItem(arr1,index1,arr2,status){
+    let tempBody = arr1[index1].body;
+    
+    arr1.splice(index1,1);
+    arr2.push({body:`${tempBody}`,status:`${status}`},);
+  
+    showTodos([...arr1,...arr2]);
+}
 
